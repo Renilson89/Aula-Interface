3df346fab3ebe1918473974819f36654a7f561c4
Renilson Marques <nilsonmarques2015@hotmail.com>
2024-02-28 11:43:15 -0400

    aula 2 - Padrão de projeto Adapter

---
+++ app/src/main/java/com/example/aninterface/AdpterPadrao.kt
@@ -0,0 +1,16 @@
+package com.example.aninterface
+
+class TomadaAntiga{
+    fun passarEnergia(){
+        println("passando energia")
+    }
+}
+class ConetorNovoPadrao{
+
+}
+
+fun main(){
+
+
+
+}
