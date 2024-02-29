d5ec99eefcb21e31235be4acd69a04ee41bfe1ec
Renilson Marques <nilsonmarques2015@hotmail.com>
2024-02-29 08:26:45 -0400

    prosseguindo no Adpter

--- app/src/main/java/com/example/aninterface/AdpterPadrao.kt
+++ app/src/main/java/com/example/aninterface/AdpterPadrao.kt
@@ -1,16 +1,52 @@
 package com.example.aninterface
 
-class TomadaAntiga{
+class TomadaAntiga(val conector: Conector){
     fun passarEnergia(){
-        println("passando energia")
-    }
+        val qtdPinos = conector.quantidadePinos()
+        if (qtdPinos == 2){
+            conector.ligarAparelho()
+            println("passando energia")
+        }else{
+            println("Esse tomada só funciona com 2 pinos")
+        }
+        }
+
 }
-class ConetorNovoPadrao{
+ class ConectorAdptador(val conetorNovoPadrao: ConetorNovoPadrao) : Conector{
+     override fun quantidadePinos(): Int {
+        return 2
+     }
+
+     override fun ligarAparelho() {
+            conetorNovoPadrao.ligarAparelho()
+     }
+
+ }
+
+interface Conector{
+    fun quantidadePinos() : Int
+    fun ligarAparelho()
+}
+
+class ConetorNovoPadrao : Conector {
+  override fun quantidadePinos() : Int{
+        return 3
+    }
+
+    override fun ligarAparelho(){
+       println("Aparelho está ligado")
+    }
 
 }
 
 fun main(){
 
+    val conetorNovoPadrao = ConetorNovoPadrao()
+
+    val conectorAdptador = ConectorAdptador(conetorNovoPadrao)
+
+    val tomadaAntiga = TomadaAntiga(conetorNovoPadrao)
+    tomadaAntiga.passarEnergia()
 
 
 }
