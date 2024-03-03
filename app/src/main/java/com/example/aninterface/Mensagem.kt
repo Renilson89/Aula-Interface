af066562e8554795ae885e7c48fbf3e75774e07c
Renilson <nilsonmarques2015@hotmail.com>
2024-03-03 10:45:35 -0400

    Linear layout

--- app/src/main/java/com/example/aninterface/Mensagem.kt
+++ app/src/main/java/com/example/aninterface/Mensagem.kt
@@ -1,7 +1,7 @@
 package com.example.aninterface
 
 data class Mensagem(
-     val nome:String,
-      val ultima:String,
-    val horario:String
+        val nome:String,
+        val ultima:String,
+        val horario:String
 )
